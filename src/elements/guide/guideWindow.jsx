aff9bc43ce807d994317877d197649b5e3410cf7
Makii117 <MakiOnPc@gmail.com>
2021-03-26 20:39:15 +0100

    Added list view for guide

--- src/elements/guide/guideWindow.jsx
+++ src/elements/guide/guideWindow.jsx
@@ -3,40 +3,30 @@ import './guideWindow.scss';
 
 export const GuideWindow = (guide) => {
    
-
 return(
     
-    <div className="guideWindow" onClick={function(event){
-        if(event.target===modal){
-            modal.style.display="none";
-        }
-    }}>
-        <div onClick={maximize}>
-       <img src={guide.image} alt="generic img lol" width="25" height="25"/>
-       <h1>{guide.title}</h1> 
+    <div className="guideWindow" onClick={
+        console.log("Hello")
+    }>
+    <div class="guide-cover"> 
+       <img src={guide.image} alt="generic img lol" width="25" height="25" class="guideIMG"/>
+       <h2>{guide.title}</h2> 
        <p>{guide.description}</p>
         
-    <div id="myModal" class="modal">
+    
     <div class="modal-content">
-    <span class="close" onClick={minimize}>&times;</span> 
+    
+   
     <p>{guide.fulldesc}</p>
-    </div>
+
     </div>
     </div>
     </div>
    
+   
 )}
-var modal = document.getElementById("myModal");
 
-const minimize = (modal)=> {
-    modal.style.display = "none";
 
-}
-const maximize = (modal)=> {
-
-    modal.style.display = "block";
-
-}
 
 
 export default GuideWindow;
