a52a43331a1ae66036b2278f3ffe3e953c8aed62
Makii117 <MakiOnPc@gmail.com>
2021-03-26 19:20:09 +0100

    Guide Cards and page

--- src/pages/Guide/Guide.jsx
+++ src/pages/Guide/Guide.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import GuideWindow from "../../elements/guide/guideWindow";
+import {data} from "./guideData.json";
+import "./Guide.scss";
 
-export const Guide = () => {
-  return <div></div>;
-};
+export const Guide = () =>{
+  
+return (
+    <>
+    
+    <div className="guide-container">
+     
+      {data.map((guide,key)=>{
+        console.log(guide)
+        return(
+
+          <div key={key}>
+      <GuideWindow id={guide.id} image={guide.image} title={guide.title}  description={guide.description} />
+      </div>
+        )  
+    })
+    
+    }
+    </div>;
+    </>
+);
+
+
+    }
+export default Guide;
